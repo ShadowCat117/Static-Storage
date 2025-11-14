750d4a7941af5d9e51b5b1e3e9b6e4d775a8dfe1
Jamie <JamCal117@gmail.com>
2025-11-14 17:43:27 +0000

    feat: Abilities v2

--- Generators/merge_abilities.sh
+++ Generators/merge_abilities.sh
@@ -8,13 +8,13 @@ OUTPUT_DIR="$BASE_DIR"/Reference
 mkdir -p "$OUTPUT_DIR"
 
 # Initialize the combined file as an empty JSON object
-COMBINED_JSON="$OUTPUT_DIR/abilities.json"
+COMBINED_JSON="$OUTPUT_DIR/abilities_v2.json"
 echo "{}" > "$COMBINED_JSON"
 
 for FILE in "$CONTENT_DIR"/*_abilities.json; do
   KEY=$(basename "$FILE" | sed -E 's/(_abilities.json)//') # Extract 'x' from 'x_abilities.json'
-  CONTENT=$(jq '.' "$FILE") # Read the entire JSON file
-  jq --arg key "$KEY" --argjson content "$CONTENT" '.[$key] = $content' "$COMBINED_JSON" > tmp.json && mv tmp.json "$COMBINED_JSON"
+
+  jq --arg key "$KEY" --slurpfile content "$FILE" '.[$key] = $content' "$COMBINED_JSON" > tmp.json && mv tmp.json "$COMBINED_JSON"
 done
 
 # Sort keys
@@ -23,6 +23,6 @@ jq 'to_entries | sort_by(.key) | from_entries' "$COMBINED_JSON" > tmp.json && mv
 # Calculate md5sum of the new combined data
 MD5=$(md5sum "$COMBINED_JSON" | cut -d' ' -f1)
 
-# Update urls.json with the new md5sum for dataStaticAbilities
-jq --arg md5 "$MD5" '. = [.[] | if (.id == "dataStaticAbilities") then .md5 = $md5 else . end]' < "$BASE_DIR"/Data-Storage/urls.json > "$BASE_DIR"/Data-Storage/urls.json.tmp
+# Update urls.json with the new md5sum for dataStaticAbilitiesV2
+jq --arg md5 "$MD5" '. = [.[] | if (.id == "dataStaticAbilitiesV2") then .md5 = $md5 else . end]' < "$BASE_DIR"/Data-Storage/urls.json > "$BASE_DIR"/Data-Storage/urls.json.tmp
 mv "$BASE_DIR"/Data-Storage/urls.json.tmp "$BASE_DIR"/Data-Storage/urls.json
